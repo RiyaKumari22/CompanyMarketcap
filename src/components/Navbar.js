4e70c299adae019703a6bbd0c67e0bc7019e2d31
Riya Kumari <riya.kumari.4rmk@gmail.com>
2023-08-17 12:41:11 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+   import React from 'react'
+ 
+   export default function Navbar() {
+     return (
+       <div>
+     <nav class="navbar bg-body-secondary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">CompanyMarketcap</span>
+  </div>
+</nav>
+       </div>
+     )
+   }
+   
+
+ 
---
+++ src/components/Screen.js
@@ -0,0 +1,161 @@
+import React from 'react'
+import "../css/screen.css"
+export default function screen() {
+  return (
+   <div className='scard'>
+   <div class="card">
+  <img src="images/tim.jpeg" class="card-img-top" alt="Tim"  />
+  <div class="card-body">
+   <p>Tim Cook
+    <br />
+    (CEO of Apple)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/satya.jpeg" class="card-img-top" alt="Satya Nadella"  />
+  <div class="card-body">
+   <p>Satya Nadella
+    <br />
+    (CEO of MICROSOFT)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/amir.jpeg" class="card-img-top" alt="Amin Hassan Naser "  />
+  <div class="card-body">
+   <p>Amin Hassan Naser 
+    <br />
+    (CEO of Saudi Aramco)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/andey.jpeg" class="card-img-top" alt="Andey Jassy"  />
+  <div class="card-body">
+   <p>Andey Jassy
+    <br />
+    (CEO of Amazon)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/jensen.jpeg" class="card-img-top" alt="Jensen Huang"  />
+  <div class="card-body">
+   <p>Jensen Huang 
+    <br />
+    (CEO of NVIDIA)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/warren.jpeg" class="card-img-top" alt="Warren Buffet"  />
+  <div class="card-body">
+   <p>Warren Buffet
+    <br />
+    (CEO of Berkshire Hathaway)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/mark.jpeg" class="card-img-top" alt="Mark Zuckerberg"  />
+  <div class="card-body">
+   <p>Mark Zuckerberg
+    <br />
+    (CEO of Meta platforms-Facebook)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/elon.jpeg" class="card-img-top" alt="Elon Musk"  />
+  <div class="card-body">
+   <p> Elon Musk
+    <br />
+    (CEO of Tesla)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/david.jpeg" class="card-img-top" alt="David A. Ricks"  />
+  <div class="card-body">
+   <p>David A. Ricks
+    <br />
+    (CEO of Eli Lilly)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/ryan.jpeg" class="card-img-top" alt="Ryan McInerney"  />
+  <div class="card-body">
+   <p>Ryan McInerney
+    <br />
+    (CEO of visa)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/wei.jpeg" class="card-img-top" alt="C.C.Wei"  />
+  <div class="card-body">
+   <p>C. C. Wei
+    <br />
+    (CEO of TSMC)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/andrew.jpeg" class="card-img-top" alt="Andrew Witty "  />
+  <div class="card-body">
+   <p>Andrew Witty 
+    <br />
+    (CEO of UnitedHealth)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/johnson.jpeg" class="card-img-top" alt="Joaquin Duato"  />
+  <div class="card-body">
+   <p>Joaquin Duato
+    <br />
+    (CEO of Johnson & Johnson)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/bernard.jpeg" class="card-img-top" alt="Bernard Arnault"  />
+  <div class="card-body">
+   <p> Bernard Arnault
+    <br />
+    (CEO of LVMH)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/jamie.jpeg" class="card-img-top" alt="Jamie Dimon"  />
+  <div class="card-body">
+   <p> Jamie Dimon
+    <br />
+    (CEO of JPMmorgan Chase)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/darren.jpeg" class="card-img-top" alt="Darren Woods"  />
+  <div class="card-body">
+   <p>  Darren Woods
+    <br />
+    (CEO of Exxon Mobil)
+   </p>
+  </div>
+</div>
+<div class="card">
+  <img src="images/doug.jpeg" class="card-img-top" alt=" Doug McMillon"  />
+  <div class="card-body">
+   <p> Doug McMillon
+    <br />
+    (CEO of Walmart)
+   </p>
+  </div>
+</div>
+   </div>
+  )
+}
